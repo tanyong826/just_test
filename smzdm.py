8162e55b64dfdee4fb5902390179c24391fede06
wd210010 <76995206+wd210010@users.noreply.github.com>
2022-12-23 11:27:16 +0800

    什么值得买签到

---
+++ smzdm.py
@@ -0,0 +1,50 @@
+import requests, json,time,hashlib
+
+#配置cookie 支持多账号一行一个 放单引号里面 根据实际需求增删
+cookie = [
+    '',
+    '',
+    ''
+]
+
+for i in range(len(cookie)):
+    print(f'开始第{i + 1}个帐号签到')
+    ts =int(round(time.time() * 1000))
+    url = 'https://user-api.smzdm.com/robot/token'
+    headers = {
+        'Host': 'user-api.smzdm.com',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Cookie': f'{cookie[i]}',
+        'User-Agent': 'smzdm_android_V10.4.1 rv:841 (22021211RC;Android12;zh)smzdmapp',
+    }
+    data={
+        "f":"android",
+        "v":"10.4.1",
+        "weixin":1,
+        "time":ts,
+        "sign":hashlib.md5(bytes(f'f=android&time={ts}&v=10.4.1&weixin=1&key=apr1$AwP!wRRT$gJ/q.X24poeBInlUJC',encoding='utf-8')).hexdigest().upper()
+    }
+    html = requests.post(url=url, headers=headers, data=data)
+    result = html.json()
+    token= result['data']['token']
+
+    Timestamp =int(round(time.time() * 1000))
+    data={
+        "f":"android",
+        "v":"10.4.1",
+        "sk":"ierkM0OZZbsuBKLoAgQ6OJneLMXBQXmzX+LXkNTuKch8Ui2jGlahuFyWIzBiDq/L",
+        "weixin":1,
+        "time":Timestamp,
+        "token":token,
+        "sign":hashlib.md5(bytes(f'f=android&sk=ierkM0OZZbsuBKLoAgQ6OJneLMXBQXmzX+LXkNTuKch8Ui2jGlahuFyWIzBiDq/L&time={Timestamp}&token={token}&v=10.4.1&weixin=1&key=apr1$AwP!wRRT$gJ/q.X24poeBInlUJC',encoding='utf-8')).hexdigest().upper()
+    }
+    url = 'https://user-api.smzdm.com/checkin'
+    headers = {
+        'Host': 'user-api.smzdm.com',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Cookie': f'{cookie[i]}',
+        'User-Agent': 'smzdm_android_V10.4.1 rv:841 (22021211RC;Android12;zh)smzdmapp',
+    }
+    html = requests.post(url=url, headers=headers, data=data)
+    result = json.loads(html.text)['error_msg']
+    print(result)
